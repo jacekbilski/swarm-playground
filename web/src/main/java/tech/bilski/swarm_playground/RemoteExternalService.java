e7074f6b30e42956784f0a187379e14dc3c0f4f3
Jacek Bilski <jacek@bilski.tech>
2023-12-04 23:16:54 +0100

    Web now uses backend, address hardcoded in code. Backend must run with Docker because of port mapping. Still no swarm.

---
+++ web/src/main/java/tech/bilski/swarm_playground/RemoteExternalService.java
@@ -0,0 +1,33 @@
+package tech.bilski.swarm_playground;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+@Service
+public class RemoteExternalService implements ExternalService {
+
+  private final WebClient webClient;
+
+  public RemoteExternalService(WebClient webClient) {
+    this.webClient = webClient;
+  }
+
+  @Override
+  public Flux<String> getMessages() {
+    return Flux.interval(Duration.of(2, ChronoUnit.SECONDS))
+        .publishOn(Schedulers.boundedElastic())
+        .concatMap(this::fetch);
+  }
+
+  private Mono<String> fetch(Long x) {
+    return webClient.get()
+        .uri("http://localhost:7070/message")
+        .retrieve()
+        .bodyToMono(String.class);
+  }
+}
--- web/src/main/java/tech/bilski/swarm_playground/SwarmPlaygroundApplication.java
+++ web/src/main/java/tech/bilski/swarm_playground/SwarmPlaygroundApplication.java
@@ -2,11 +2,18 @@ package tech.bilski.swarm_playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class SwarmPlaygroundApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SwarmPlaygroundApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SwarmPlaygroundApplication.class, args);
+  }
+
+  @Bean
+  WebClient webClient() {
+    return WebClient.builder().build();
+  }
 }
