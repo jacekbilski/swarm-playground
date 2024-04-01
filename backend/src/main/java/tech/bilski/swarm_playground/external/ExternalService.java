5cdb5fe82b1d5829187ebc32dc5b984563e51bfd
Jacek Bilski <jacek@bilski.tech>
2024-04-01 19:53:22 +0200

    Logging hostname and replica of a backend service responding to the request. Disappointing, only one replica is responding, no round-robin.

--- backend/src/main/java/tech/bilski/swarm_playground/external/ExternalService.java
+++ backend/src/main/java/tech/bilski/swarm_playground/external/ExternalService.java
@@ -1,12 +1,24 @@
 package tech.bilski.swarm_playground.external;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class ExternalService {
 
-    public Mono<String> getMessage() {
-        return Mono.just("a");
-    }
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+  private final String hostname;
+  private final String replica;
+
+  public ExternalService() {
+    hostname = System.getenv("HOSTNAME");
+    replica = System.getenv("REPLICA");
+  }
+
+  public Mono<String> getMessage() {
+    log.info("ExternalService[h:{},r:{}].message", hostname, replica);
+    return Mono.just(String.format("ExternalService (external, h: '%s', r: '%s') sends greetings!", hostname, replica));
+  }
 }
