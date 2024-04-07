d19a5221e4e2a5b5baefbd4fb66bb5dd845a8305
Jacek Bilski <jacek@bilski.tech>
2024-04-07 12:57:35 +0200

    Simple persisting a list of messages in a session.

--- web/src/main/java/tech/bilski/swarm_playground/sessions/SessionsController.java
+++ web/src/main/java/tech/bilski/swarm_playground/sessions/SessionsController.java
@@ -1,10 +1,12 @@
 package tech.bilski.swarm_playground.sessions;
 
+import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.result.view.Rendering;
 import org.springframework.web.server.WebSession;
 
@@ -21,6 +23,18 @@ public class SessionsController {
     return Rendering
         .view("sessions")
         .modelAttribute("sessionId", session.getId())
+        .modelAttribute("messages", session.getAttributeOrDefault("messages", new ArrayList<String>()))
+        .build();
+  }
+
+  @GetMapping("/addMessage")
+  public Rendering addMessage(@RequestParam("message") String message, WebSession session) {
+    log.info("Got message: {}", message);
+    var messages = session.getAttributeOrDefault("messages", new ArrayList<String>());
+    messages.add(message);
+    session.getAttributes().put("messages", messages);
+    return Rendering
+        .redirectTo("/sessions")
         .build();
   }
 }
