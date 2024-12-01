e345705ed7aab0777e38ca220e7ccd5a24ad247d
Jacek Bilski <jacek@bilski.tech>
2024-12-01 19:37:52 +0100

    Spring Boot upgrade.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.springframework.boot") version "3.2.4" apply false
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.4.0" apply false
+    id("io.spring.dependency-management") version "1.1.6"
     java
 }
 
